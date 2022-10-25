e250bdb98cefbbded608227aaeb7ccee2eaa8e16
zouchangfu <zcf13543354971@163.com>
2022-10-25 10:00:18 +0800

    slice 容量问题

--- interview/gopher_test.go
+++ interview/gopher_test.go
@@ -337,3 +337,32 @@ func TestJson4(t *testing.T) {
 		fmt.Printf("[%v] result => %+v\n", idx, result)
 	}
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// slice重新分配问题
+func get() []byte {
+	raw := make([]byte, 10000)
+	fmt.Println(len(raw), cap(raw), &raw[0]) // 10000 10000 0xc420080000
+	return raw[:3]                           // 重新分配容量为 10000 的 slice
+}
+
+func TestSliceGet(t *testing.T) {
+	data := get()
+	fmt.Println(len(data), cap(data), &data[0]) // 3 10000 0xc420080000
+	// 这里的data的容量大小为10000
+}
+
+// 正确的方式，应该是通过copy来实现
+func copyData() (res []byte) {
+	raw := make([]byte, 10000)
+	fmt.Println(len(raw), cap(raw), &raw[0]) // 10000 10000 0xc420080000
+	res = make([]byte, 3)
+	copy(res, raw[:3])
+	return
+}
+
+func TestSliceCopy(t *testing.T) {
+	data := copyData()
+	fmt.Println(len(data), cap(data), &data[0])
+	//3 3 0xc00001ce00
+}
