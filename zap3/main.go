e7c923fe4abf3c67287c3349cc843a7486750377
zouchangfu <zcf13543354971@163.com>
2022-10-29 21:51:08 +0800

    增加zap3 格式化打印日志

---
+++ zap3/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	logTmFmt = "2006-01-02 15:04:05"
+)
+
+func main() {
+	GetLogger()
+	r := gin.New()
+	r.Use(GinLogger())
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"version": "v1.1",
+		})
+	})
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "404",
+		})
+	})
+	r.Run(":9090")
+}
+func GinLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		zap.L().Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
+	}
+}
+
+func GetLogger() {
+	Encoder := GetEncoder()
+	WriteSyncer := GetWriteSyncer()
+	LevelEnabler := GetLevelEnabler()
+	ConsoleEncoder := GetConsoleEncoder()
+	newCore := zapcore.NewTee(
+		zapcore.NewCore(Encoder, WriteSyncer, LevelEnabler),                          // 写入文件
+		zapcore.NewCore(ConsoleEncoder, zapcore.Lock(os.Stdout), zapcore.DebugLevel), // 写入控制台
+	)
+	logger := zap.New(newCore, zap.AddCaller())
+	zap.ReplaceGlobals(logger)
+}
+
+// GetEncoder 自定义的Encoder
+func GetEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(
+		zapcore.EncoderConfig{
+			TimeKey:        "ts",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller_line",
+			FunctionKey:    zapcore.OmitKey,
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     "  ",
+			EncodeLevel:    cEncodeLevel,
+			EncodeTime:     cEncodeTime,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   cEncodeCaller,
+		})
+}
+
+// GetConsoleEncoder 输出日志到控制台
+func GetConsoleEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+}
+
+// GetWriteSyncer 自定义的WriteSyncer
+func GetWriteSyncer() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./zap.log",
+		MaxSize:    200,
+		MaxBackups: 10,
+		MaxAge:     30,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+// GetLevelEnabler 自定义的LevelEnabler
+func GetLevelEnabler() zapcore.Level {
+	return zapcore.InfoLevel
+}
+
+// cEncodeLevel 自定义日志级别显示
+func cEncodeLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + level.CapitalString() + "]")
+}
+
+// cEncodeTime 自定义时间格式显示
+func cEncodeTime(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + t.Format(logTmFmt) + "]")
+}
+
+// cEncodeCaller 自定义行号显示
+func cEncodeCaller(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + caller.TrimmedPath() + "]")
+}
